18819fd5714e210dd7a094408d0740f5d10f0ba8
ji3g4vul3eji <41719417+ji3g4vul3eji@users.noreply.github.com>
2018-08-16 11:34:30 +0800

    Initial commit

---
+++ C Sharp Warmup/C Sharp Warmup/NumAdding.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace C_Sharp_Warmup
+{
+    public class NumAdding
+    {
+        int i = 1;
+        int sum = 0;
+        public int AddingByWhile()
+        {
+            while (i < 100)
+            {
+                sum += i;
+                i += 2;
+            }
+            return sum;
+        }
+        public int AddingByFor()
+        {
+            sum = 0;
+            for (i = 1; i < 100; i += 2)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public int AddingByDo()
+        {
+            i = 1;
+            sum = 0;
+            do
+            {
+                sum += i;
+                i += 2;
+            }
+            while (i < 100);
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumAdding W = new NumAdding();
+            Console.WriteLine(W.AddingByWhile());
+            Console.WriteLine(W.AddingByFor());
+            Console.WriteLine(W.AddingByDo());
+        }
+    }
+}
